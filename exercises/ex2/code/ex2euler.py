2941c8c7c74809fe513fd27b5d0041eeccb1d222
einar90 <einar.baumann@gmail.com>
2014-02-03 22:45:06 +0100

    Ex2 complete

--- exercises/ex2/code/ex2euler.py
+++ exercises/ex2/code/ex2euler.py
@@ -6,7 +6,7 @@ nmax = (int)(round(endx/dx)) # Largest n
 
 # Defining ddy function
 def ddy(yn):
-  return 2*yn*(1-3*yn/2)
+    return 2.0*yn*(1.0-3.0*yn/2.0)
 
 
 # Initializing arrays
@@ -14,11 +14,11 @@ y   = [];
 dy  = [];
 
 # Setting initial values
-y.append(1)
-dy.append(0)
+y.append(1.0)
+dy.append(0.0)
 
-for n in range(0,nmax+1):
-  yn = y[-1]; dyn = dy[-1]
-  dy.append(dyn+dx*ddy(yn))
-  y.append(yn+dx*dyn)
-  print("%d \t\t x=%f \t\t y=%f \t\t dy=%f" % (n, 0.2*n, y[-1], dy[-1]))
+for n in range(1,nmax+1):
+    yn = y[-1]; dyn = dy[-1]
+    dy.append(dyn+dx*ddy(yn))
+    y.append(yn+dx*dyn)
+    print("%d \t\t x=%f \t\t y=%f \t\t dy=%f" % (n, 0.2*n, y[-1], dy[-1]))
--- exercises/ex2/code/ex2heun.py
+++ exercises/ex2/code/ex2heun.py
@@ -4,9 +4,10 @@ dx = 0.2                     # Step
 endx = 0.6                   # Last x-value
 nmax = (int)(round(endx/dx)) # Largest n
 
+
 # Defining ddy function
 def ddy(yn):
-  return 2*yn*(1-3*yn/2)
+    return 2.0*yn*(1.0-3.0*yn/2.0)
 
 
 # Initializing arrays
@@ -16,8 +17,17 @@ yp  = [];
 dyp = [];
 
 # Setting initial values
-y.append(1)
-dy.append(0)
+y.append(1.0)
+dy.append(0.0)
 
-for n in range(0,nmax+1):
+for n in range(1,nmax+1):
+    yn = y[-1]
+    dyn = dy[-1]
+
+    dyp.append(dyn + dx*ddy(yn))
+    yp.append( yn  + dx*dyn)
+
+    dy.append( dyn + dx/2.0 * (ddy(yn) + ddy(yp[-1])))
+    y.append(  yn  + dx/2.0 * (dyn     + dyp[-1]))
+    print("%d \t x=%f \t yp=%f \t dyp=%f \t y=%f \t dy=%f" % (n, 0.2*n, yp[-1], dyp[-1], y[-1], dy[-1]))
 
